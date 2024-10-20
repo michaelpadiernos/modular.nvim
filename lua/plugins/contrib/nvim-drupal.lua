988012c254f7190e04e694a644bd15716cbc7621
M. Padiernos <mike@padiernos.me>
2024-10-20 03:31:52 -0400

    Update nvim-drupal.lua

--- lua/plugins/contrib/nvim-drupal.lua
+++ lua/plugins/contrib/nvim-drupal.lua
@@ -8,7 +8,7 @@ return {
             version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
             -- install jsregexp (optional!).
             build = "make install_jsregexp"
-        ,}
+        }
     },
     config = function()
     require("drupal").setup({
