dfcf3e5a106353b3d6cd55023011619fa16a8bc0
Michael Padiernos <mike@padiernos.me>
2024-10-19 22:22:52 -0400

    [theme] github-theme / added custom colors

--- lua/themes/nvim-onedarkpro.lua
+++ lua/themes/nvim-onedarkpro.lua
@@ -5,8 +5,8 @@ return {
     config = function()
         vim.cmd("colorscheme onedark_dark")
 
-        vim.api.nvim_set_hl(0, 'NeominimapBorder', { fg = '#000000' })
-        vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#000000', bg = '#000000' })
+        vim.api.nvim_set_hl(0, 'NeominimapBorder',  { fg = '#000000' })
+        vim.api.nvim_set_hl(0, 'WinSeparator',      { fg = '#000000', bg = '#000000' })
 
         require("onedarkpro").setup({
             options = {
