b9bc28a3101fde8cf0448056b2ba7dbb98e03852
Michael Padiernos <mike@padiernos.me>
2024-10-22 15:33:57 -0400

    [build] added some plugins to test

--- lua/core/extend.lua
+++ lua/core/extend.lua
@@ -1,33 +1,36 @@
-require('lazy').setup({
+require('lazy').setup {
 
-    -- Plugins
-    require 'plugins',
+  -- Plugins
+  require 'plugins',
 
-    -- Plugins: configs
-    { import = 'plugins.mini' },
-    { import = 'plugins.contrib' },
-    { import = 'plugins.vim' },
-    { import = 'plugins.drupal' },
+  -- Plugins: configs
+  { import = 'plugins.mini' },
+  { import = 'plugins.contrib' },
+  { import = 'plugins.vim' },
+  { import = 'plugins.drupal' },
 
-    -- Themes
-    { enabled = true,  'projekt0n/github-nvim-theme' },
-    { enabled = false, 'olimorris/onedarkpro.nvim' },
+  -- Themes
+  { enabled = true, 'projekt0n/github-nvim-theme' },
+  { enabled = false, 'olimorris/onedarkpro.nvim' },
 
-    -- Themes: configs
-    { import = 'themes' },
+  -- Themes: configs
+  { import = 'themes' },
 
-    -- LSP
-    { enabled = true,   'williamboman/mason.nvim' },
-    { enabled = true,   'williamboman/mason-lspconfig.nvim' },
+  -- LSP
+  { enabled = true, 'williamboman/mason.nvim' },
+  { enabled = true, 'williamboman/mason-lspconfig.nvim' },
 
-    -- LSP: extensions
-    { import = 'lsp' },
+  -- LSP: extensions
+  { import = 'lsp' },
 
-    -- Completions
-    { enabled = true,	'hrsh7th/nvim-cmp'},
-    { enabled = true,  'petertriho/cmp-git' },
-    { enabled = true,  'fbuchlak/cmp-symfony-router'},
+  -- Completions
+  { enabled = true, name = 'cmp', 'hrsh7th/nvim-cmp' },
+  { enabled = true, name = 'cmp-buffer', 'hrsh7th/cmp-buffer' },
+  { enabled = true, name = 'cmp-cmdline', 'hrsh7th/cmp-cmdline' },
+  { enabled = true, name = 'cmp-git', 'petertriho/cmp-git' },
+  { enabled = true, name = 'cmp-path', 'hrsh7th/cmp-path' },
+  { enabled = true, name = 'cmp-symfony-router', 'fbuchlak/cmp-symfony-router' },
 
-    -- Completions: configs
-    { import = 'completions' },
-})
+  -- Completions: configs
+  { import = 'completions' },
+}
