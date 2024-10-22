721da7d9c6a05620f9d9bbe79c1968f345cf2631
Michael Padiernos <mike@padiernos.me>
2024-10-22 14:37:27 -0400

    [layout] added default format to active buffer for mini.statusline

--- lua/plugins/mini/mini-ai.lua
+++ lua/plugins/mini/mini-ai.lua
@@ -1,6 +1,6 @@
 return {
   'echasnovski/mini.ai',
-  version = true,
+  version = false,
   enabled = true,
   config = function()
     require('mini.ai').setup {
--- lua/plugins/mini/mini-statusline.lua
+++ lua/plugins/mini/mini-statusline.lua
@@ -11,15 +11,30 @@ return {
       -- `:h statusline` and code of default contents (used instead of `nil`).
       content = {
         active = function()
+          local mode, mode_hl = MiniStatusline.section_mode { trunc_width = 120 }
+          local git = MiniStatusline.section_git { trunc_width = 40 }
+          local diff = MiniStatusline.section_diff { trunc_width = 75 }
+          local diagnostics = MiniStatusline.section_diagnostics { trunc_width = 75 }
+          local lsp = MiniStatusline.section_lsp { trunc_width = 75 }
+          local filename = MiniStatusline.section_filename { trunc_width = 140 }
+          local fileinfo = MiniStatusline.section_fileinfo { trunc_width = 120 }
+          local location = MiniStatusline.section_location { trunc_width = 75 }
+          local search = MiniStatusline.section_searchcount { trunc_width = 75 }
+
           if blocked[vim.bo.filetype] then
             return ''
           else
-            return MiniStatusline.section_mode {
-              n = 'n',
+            return MiniStatusline.combine_groups {
+              { hl = mode_hl, strings = { mode } },
+              { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+              '%<', -- Mark general truncate point
+              { hl = 'MiniStatuslineFilename', strings = { filename } },
+              '%=', -- End left alignment
+              { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+              { hl = mode_hl, strings = { search, location } },
             }
           end
         end,
-        -- active = nil,
         inactive = function()
           return ''
         end,
